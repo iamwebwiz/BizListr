8419fc1a45804299d5541ce9046928fd424f9f78
Ezekiel <iamwebwiz@gmail.com>
2020-05-23 07:02:20 +0100

    Add business category pivot migration

---
+++ database/migrations/2020_05_23_055239_create_business_category_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_category', static function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('business_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('business_id')
+                ->references('id')
+                ->on('businesses')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business_category');
+    }
+}
