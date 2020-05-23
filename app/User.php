36dd6faead2a3ea3c20a4279dfa0cd6c582397fa
Ezekiel <iamwebwiz@gmail.com>
2020-05-23 07:01:38 +0100

    Update User model

--- app/User.php
+++ app/User.php
@@ -10,6 +10,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public const USER_ROLE = 'user';
+
+    public const ADMIN_ROLE = 'admin';
+
     /**
      * The attributes that are mass assignable.
      *
